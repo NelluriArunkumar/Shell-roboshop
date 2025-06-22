a9b2c9370340a4382ea738d00c89df93ad3d51b7
NArunkumar <nelluri.arunkumar@gmail.com>
2025-06-22 14:14:00 +0530

    RoboShop-Automation through shell

---
+++ catalogue.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+USERID=$(id -u) 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs" ##Here we are creating the roboshop logs directory
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) ##Here we are getting the file name with '$0' and removing the .sh extension 
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  ##Here we are denoting the logs file name to LOG_FILE Variable..like 14-logs.log
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling the nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling the nodejs 20 version"
+
+dnf install nodejs -y
+VALIDATE $? "Installing the nodejs"
+
+id roboshop
+
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating the roboshop system user"
+else
+    echo -e "System user roboshop already created... $Y Skipping $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Downloading the Catalogue"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzipping the catalogue"
+
+npm install 
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+VALIDATE $? "Starting the Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB Client"
+
+
+STATUS=$(mongosh --host mongodb.arunkumarnelluri.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ STATUS -lt 0 ]
+then
+    mongosh --host mongodb.arunkumarnelluri.site </app/db/master-data.js
+    VALIDATE $? "Loading data into mongo db"
+else
+    echo -e "Data is already loaded...$Y SKIPPING $N"
+fi
